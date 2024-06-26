2d51c8b7ec63f18dfbf829d9a5c87311bbf2ae09
chaksw <runsheng.wu@outlook.com>
2024-06-27 06:51:07 +0800

    update django scgasite

--- scgasite/scgaapi/serializers.py
+++ scgasite/scgaapi/serializers.py
@@ -7,8 +7,8 @@ class TestPlanSerializer(serializers.ModelSerializer):
         model = TestPlan
         fields = (
             'id',
-            'scga_file',
-            'sheet_name',
+            # 'scga_file',
+            # 'sheet_name',
             'level'
         )
 
@@ -18,15 +18,16 @@ class TestExceptionSerializer(serializers.ModelSerializer):
         model = TestException
         fields = (
             'id',
-            'scga_file',
-            'sheet_name',
+            # 'scga_file',
+            # 'sheet_name',
             'level',
         )
 
 
 class LevelSerializer(serializers.ModelSerializer):
-    test_plans = TestPlanSerializer(many=True)
-    test_exceptions = TestExceptionSerializer(many=True)
+    # one to one: One level has only one test plan and one test exception
+    test_plans = TestPlanSerializer()
+    test_exceptions = TestExceptionSerializer()
 
     class Meta:
         model = Level
@@ -36,6 +37,20 @@ class LevelSerializer(serializers.ModelSerializer):
             'test_plans',
             'test_exceptions'
         )
+
+    def create(self, validated_data):
+        print('get there4')
+        test_plans_data = validated_data.pop("test_plans")
+        test_execeptions_data = validated_data.pop("test_exceptions")
+        # ** is destructure symbol of dictionary
+        level = Level.objects.create(**validated_data)
+        if test_plans_data is not None:
+            for test_plan_data in test_plans_data:
+                TestPlan.objects.create(level=level, **test_plan_data)
+        for test_exeception_data in test_execeptions_data:
+            if test_execeptions_data is not None:
+                TestException.objects.create(level=level, **test_exeception_data)
+        return level
 
 
 class ScgaSerializer(serializers.ModelSerializer):
@@ -54,3 +69,31 @@ class ScgaSerializer(serializers.ModelSerializer):
             # 'test_plans',
             # 'test_exceptions'
         )
+
+    # override .create() function
+    # ** is destructure symbol of dictionary
+    def create(self, validated_data):
+        print(validated_data)
+        print('get there1')
+        levels_data = validated_data.pop("levels")
+        scga = Scga.objects.create(**validated_data)
+        if levels_data is not None:
+            print(levels_data)
+            print('get there2')
+            for level_data in levels_data:
+                test_plans_data = level_data.pop("test_plans")
+                # strat from here
+                print(test_plans_data)
+                test_execeptions_data = level_data.pop("test_exceptions")
+                print(test_execeptions_data)
+                # ** is destructure symbol of dictionary
+                level = Level.objects.create(**level_data)
+                print('get there3')
+                if test_plans_data is not None:
+                    for test_plan_data in test_plans_data:
+                        TestPlan.objects.create(level=level, **test_plan_data)
+                for test_exeception_data in test_execeptions_data:
+                    if test_execeptions_data is not None:
+                        TestException.objects.create(level=level, **test_exeception_data)
+                print('get there4')
+        return scga
