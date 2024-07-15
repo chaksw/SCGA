dbc2ed9102ea2c0879d24213ab27a08a6610c21a
chaksw <runsheng.wu@outlook.com>
2024-07-15 22:32:42 +0800

    update gitignore

--- frontend/scga_vue/src/main.js
+++ frontend/scga_vue/src/main.js
@@ -1,6 +1,9 @@
 import './assets/main.css'
-
+import axios from 'axios'
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// setting a default URL to response
+axios.defaults.baseURL = 'httop://127.0.0.1:8000'
 
 createApp(App).mount('#app')
